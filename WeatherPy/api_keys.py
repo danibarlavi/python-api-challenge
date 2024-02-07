434513e2b2cf1489171fcd658ba69f6edbbc7632
danibarlavi <danibarlavi@gmail.com>
2024-02-06 23:15:05 -0600

    Finished project

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = '28713449e8f952c70438beb3dc842f34'
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "d79328d9cd6b4e7cb3c6a1d40b410c33"
